15b17fd804535dfb5961995c7a02d07525b1fd35
kationic <cuaycongkatrina@gmail.com>
2026-04-13 07:47:53 +0100

    Replace email notifications with SMS alerts
    Refactor code to use Twilio for SMS notifications and remove email functionality.

--- main.py
+++ main.py
@@ -1,38 +1,43 @@
-# To run and test the code you need to update 4 places:
-# 1. Change MY_EMAIL/MY_PASSWORD to your own details.
-# 2. Go to your email provider and make it allow less secure apps.
-# 3. Update the SMTP ADDRESS to match your email provider.
-# 4. Update birthdays.csv to contain today's month and day.
-# See the solution video in the 100 Days of Python Course for explainations.
-
-
-from datetime import datetime
-import pandas
-import random
-import smtplib
+import requests
+from twilio.rest import Client
 import os
 
-# import os and use it to get the Github repository secrets
-MY_EMAIL = os.environ.get("MY_EMAIL")
-MY_PASSWORD = os.environ.get("MY_PASSWORD")
+LATITUDE = 51.588187
+LONGITUDE = -0.042389
+API_KEY = os.environ.get("OWM_API_KEY") 
+OWM_ENDPOINT = "https://api.openweathermap.org/data/2.5/forecast"
+INTERVAL = 4
+ACCOUNT_SID= os.environ.get("TWIL_ACCOUNT_SID")
+AUTH_TOKEN= os.environ.get("TWIL_AUTH_TOKEN")
+MY_NUMBER= os.environ.get("PERS_NUMBER")
 
-today = datetime.now()
-today_tuple = (today.month, today.day)
+PARAMS = {
+    "lat": LATITUDE,
+    "lon": LONGITUDE,
+    "appid": API_KEY,
+    "cnt": INTERVAL
+}
 
-data = pandas.read_csv("birthdays.csv")
-birthdays_dict = {(data_row["month"], data_row["day"])                  : data_row for (index, data_row) in data.iterrows()}
-if today_tuple in birthdays_dict:
-    birthday_person = birthdays_dict[today_tuple]
-    file_path = f"letter_templates/letter_{random.randint(1, 3)}.txt"
-    with open(file_path) as letter_file:
-        contents = letter_file.read()
-        contents = contents.replace("[NAME]", birthday_person["name"])
+response = requests.get(OWM_ENDPOINT, params=PARAMS)
+response.raise_for_status()
 
-    with smtplib.SMTP("YOUR EMAIL PROVIDER SMTP SERVER ADDRESS") as connection:
-        connection.starttls()
-        connection.login(MY_EMAIL, MY_PASSWORD)
-        connection.sendmail(
-            from_addr=MY_EMAIL,
-            to_addrs=birthday_person["email"],
-            msg=f"Subject:Happy Birthday!\n\n{contents}"
-        )
+weather_data = response.json()
+
+count=0
+will_rain = False
+for instance in weather_data["list"]:
+    if instance["weather"][0]["id"] <700:
+        will_rain = True
+    else:
+        continue
+    count += 1
+
+if will_rain:
+    client = Client(ACCOUNT_SID, AUTH_TOKEN)
+
+    message = client.messages.create(
+        from_="+447426933513",
+        to=MY_NUMBER,
+        body = "Bring an ☂️ today!"
+    )
+    print(message.sid)
